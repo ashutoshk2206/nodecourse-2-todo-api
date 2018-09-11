cd87381d592632f015d94ae69dd353e7b0e1ba0b
AshutoshKumar <ashutosh.kumar2@mindtree.com>
2018-09-11 10:29:11 +0530

    update js added

---
+++ playground/mongodb-update.js
@@ -0,0 +1,40 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true}, (err, client) => {
+    if(err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB Server');
+
+    const db = client.db('TodoApp');
+    
+    // db.collection('Todos').findOneAndUpdate({
+    //     completed: false
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        name: 'Rajendra'
+    }, {
+        $set: {
+            name: 'Neel'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+    client.close();
+});
